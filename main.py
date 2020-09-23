4379645135f19e60b297e7c8488ec479afdb0585
Samuel Chaput <samuelchaput87@gmail.com>
2020-09-23 14:00:39 +0200

    Pandas faster.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
 import re
 import time
+import hashlib
+import pandas as pd
 from typing import List
 
 
@@ -12,22 +15,43 @@ class Parser:
         self.filepath = filepath or Parser.DEFAULT_FILEPATH
         self.output_path = Parser.DEFAULT_OUTPUT
 
-    def remove_duplicates(self):
+    def remove_duplicates(self, method="BASIC"):
         """
         Remove line duplicates
         :return:
         """
-        results = []
-        lines_seen = []
-        with open(self.filepath, "r") as file:
-            lines = file.readlines()
-            for i, line in enumerate(lines):
-                l = " ".join(line.split()[4:])
-                # print(i)
-                # if l not in lines_seen[max(0, i-5000):]:
-                #     results.append(line)
-                lines_seen.append(l)
-        self.write_lines(results)
+        if method == "BASIC":
+            results = []
+            lines_seen = []
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+                for i, line in enumerate(lines):
+                    l = " ".join(line.split()[4:])
+                    if l not in lines_seen[max(0, i-5000):]:
+                        results.append(line)
+                    lines_seen.append(l)
+            self.write_lines(results)
+
+        elif method=="HASH":
+            results = []
+            hash_seen = []
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+                for i, line in enumerate(lines):
+                    l = " ".join(line.split()[4:])
+                    hashval = int(hashlib.sha1(l.encode()).hexdigest(), 16)
+                    if hashval not in hash_seen[max(0, i - 5000):]:
+                        results.append(line)
+                    hash_seen.append(hashval)
+            self.write_lines(results)
+
+        elif method=="PANDAS":
+            with open(self.filepath, "r") as file:
+                lines = file.readlines()
+                df = pd.DataFrame(data={"lines":lines, "splitted": [" ".join(line.split()[4:]) for line in lines]})
+                df.drop_duplicates(subset=["splitted"], inplace=True)
+                self.write_lines(df["lines"])
+                # df.to_csv("output.log", header=False, index=False, columns=["lines"], quoting=csv.QUOTE_NONE, escapechar='')
 
     def get_duplicates(self):
         """
@@ -54,5 +78,17 @@ class Parser:
 
 
 p = Parser()
-p.remove_duplicates()
+
+start = time.time()
+p.remove_duplicates(method="PANDAS")
+print("PANDAS method took : ", time.time()-start, "s")
+
+# start = time.time()
+# p.remove_duplicates(method="BASIC")
+# print("BASIC method took : ", time.time()-start, "s")
+#
+# start = time.time()
+# p.remove_duplicates(method="HASH")
+# print("HASH method took : ", time.time()-start, "s")
+
 print("end.")
